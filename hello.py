5583476faf2b2319b63b5b6a4205d86728cdd9f9
kpoiqs <kpoiqq@naver.com>
2022-08-05 21:49:36 +0900

    new commit

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 from mycons import Constants
+from configparser import ConfigParser
 
+config = ConfigParser()
+config.read('conf.ini')
+print(config['1st_section']['1st_key']) # value_1
 print("hello")
 print(Constants.hello)
